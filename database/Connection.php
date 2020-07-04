3d011552a61b624140cfb462f8ee9041bd6c6c59
Thwoi Hla Ching Chak <thwoi.trinarylab@gmail.com>
2020-07-04 23:46:57 +0600

    Hide Your Secret Passwords

--- database/Connection.php
+++ database/Connection.php
@@ -3,10 +3,15 @@
 
 class Connection
 {
-    public static function make()
+    public static function make($config)
     {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'root');
+            return new PDO(
+                $config['connection'] . ';dbname=' . $config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
         } catch (Exception $exception) {
             die($exception->getMessage());
         }
