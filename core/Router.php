39761cc05dbd91ceeffdb8013cd03f3100aab449
Thwoi Hla Ching Chak <thwoi.trinarylab@gmail.com>
2020-07-05 12:54:44 +0600

    Forms, Request Types, and Routing

--- core/Router.php
+++ core/Router.php
@@ -3,14 +3,19 @@
 
 class Router
 {
-    protected $routes = [];
+    public $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
 
     /**
-     * @return array
+     * @param $file
+     * @return static
      */
     public static function load($file)
     {
         $router = new static();
+
         require $file;
 
         return $router;
@@ -21,12 +26,22 @@ class Router
         $this->routes = $routes;
     }
 
-    public function direct($uri)
+    public function direct($uri, $requestType)
     {
-        if (array_key_exists($uri, $this->routes)) {
-            return $this->routes[$uri];
+        if (array_key_exists($uri, $this->routes[$requestType])) {
+            return $this->routes[$requestType][$uri];
         }
 
         throw new Exception('No route defined');
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->routes['GET'][$uri] = $controller;
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->routes['POST'][$uri] = $controller;
     }
 }
--- views/index.view.php
+++ views/index.view.php
@@ -1,21 +1,27 @@
 <?php require('partials/head.php') ?>
 
-<ul>
-    <h1>Tasks for the day</h1>
+    <!--<ul>
+        <h1>Tasks for the day</h1>
 
-    <?php foreach ($tasks as $task): ?>
+        <?php /*foreach ($tasks as $task): */ ?>
 
-        <?php if ($task->completed): ?>
-            <strike>
-        <?php endif; ?>
-        <li>
-            <?= $task->description ?>
-        </li>
-        <?php if ($task->completed): ?>
-            </strike>
-        <?php endif; ?>
-    <?php endforeach; ?>
+            <?php /*if ($task->completed): */ ?>
+                <strike>
+            <?php /*endif; */ ?>
+            <li>
+                <? /*= $task->description */ ?>
+            </li>
+            <?php /*if ($task->completed): */ ?>
+                </strike>
+            <?php /*endif; */ ?>
+        <?php /*endforeach; */ ?>
 
-</ul>
+    </ul>-->
+
+    <h2>Submit Your Name</h2>
+    <form action="/name" method="post">
+        <input type="text" name="name">
+        <button type="submit">Submit</button>
+    </form>
 
 <?php require('partials/footer.php') ?>
